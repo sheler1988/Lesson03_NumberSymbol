7c00fe3d724a2ccbf39e327896fbafddbc6fcc0a
sheler1988 <93482890+sheler1988@users.noreply.github.com>
2024-10-12 17:57:07 +0330

    1

---
+++ NumberSymbol/NumberSymbol/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+// برنامه ای بنویسید که یک عدد را از ورودی دریافت بکند و علامت آن را تعیین بکند
+
+Console.Write("Please enter number: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+if (Number1 >= 0)
+	Console.WriteLine("Namber Is Positive");
+else
+	Console.WriteLine("Namber Is Negative");
+
+Console.ReadKey();
+
+// نوشتن این برنامه در این حالت بسیار بهینه و کوتاه است اما اگر بخواهید از
+// else if
+// استفاده بکنید میتوانید از کد زیر استفاده بکنید
+
+Console.Write("Please enter number: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+if (Number2 > 0)
+	Console.WriteLine("Namber Is Positive");
+else if (Number2 == 0)
+	Console.WriteLine("Namber Is Zero");
+else if (Number2 < 0)
+	Console.WriteLine("Namber Is Negative");
+
+Console.ReadKey();
